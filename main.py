7108698a9f5a8f27559008c00a353abe836a214d
laerty505 <laertybatista20@gmail.com>
2026-05-14 22:26:25 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("-----PEDRA, PAPEL E TESOURA-----")
+print("Escolha uma Opção!")
+
+print("1 - Um Jogador")
+print("2 - Dois Jogadores")
+print("3 - Sair")
+
+sair = False
+
+while sair == False:
+
+    escolha_inicial = int(input("Digite um número para começar! "))
+
+    match escolha_inicial:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            print("Jogo Encerrado!")
+            sair = True
+        case _:
+            print("Digite um número válido!")
